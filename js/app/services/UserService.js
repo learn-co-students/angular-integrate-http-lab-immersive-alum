9cce29a0d045010ae54e1d66b022b6ee19aa36fc
Matthew Talbot <matthewtalbot@nypl.org>
2019-08-22 15:21:37 -0400

    Done.

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,5 +1,11 @@
 function UserService($http) {
+	this.getUser = () => {
+		return $http.get('http://0.0.0.0:8882/rest/user')
+	}
 
+	this.updateUser = () => {
+		return $http.post('http://0.0.0.0:8882/rest/user')
+	}
 }
 
 angular
