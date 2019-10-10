604d4e40f5a77653a4e9dd624a52be09dfd6f5c3
Rebecca Huseyin <rebecca_huseyin@outlook.com>
2019-10-10 15:30:48 +0000

    Done.

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,5 +1,11 @@
 function UserService($http) {
-
+  this.getUser = function() {
+    return $http.get('http://0.0.0.0:8882/rest/user');
+  };
+  
+  this.updateUser = function(userInfo) {
+    return $http.post('http://0.0.0.0:8882/rest/user', userInfo);
+  };
 }
 
 angular
